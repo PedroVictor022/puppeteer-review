6eb7569752b31e1dd0dd5e12ade1308828f02b7f
Pedro Victor <pedro20002victor@gmail.com>
2022-11-28 19:42:02 -0300

    Collect specific infos

--- index.js
+++ index.js
@@ -11,15 +11,26 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
   console.log('Browser init');
 
-  await page.goto('https://www.fctables.com/teams/palmeiras-191491/');
+  await page.goto('https://www.fctables.com/teams/atletico-mg-180615/');
   // await page.screenshot({ path: 'print.png' }) // Print this page
 
   const teamInfo = await page.$$('#team-info');
 
   // Collect all info of website
   for(const infos of teamInfo) {
-    const teamName = await page.evaluate(el => el.innerText, infos);
-    console.table(teamName);
+    const allInfos = await page.evaluate(el => el.innerText, infos); // Return content this website
+
+    // Return especific info 
+    const nomeTime = await page.evaluate(el => el.querySelector("h2 > span").textContent, infos);
+
+    const nextOpp = await page.evaluate(el => el.querySelector("div:nth-child(6) > a:nth-child(3)").textContent, infos);
+
+    console.log(`
+      Team name: ${nomeTime}\n
+      ----------------- \n
+      Next opponent: ${nextOpp}
+    `);
+
   };
 
   await browser.close();
