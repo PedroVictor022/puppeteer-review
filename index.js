79331aa3161c814e2bb8273bf32e0747dd0451cc
Pedro Victor <pedro20002victor@gmail.com>
2022-11-28 09:47:44 -0300

    Collect this page info

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const puppeteer = require("puppeteer");
 (async () => {
   
   const browser = await puppeteer.launch({
-    headless: true
+    headless: true,
+    defaultViewport: false,
+    userDataDir: "./tmp"
+
   });
   const page = await browser.newPage();
   console.log('Browser init');
 
-  await page.goto('https://github.com/')
-  await page.screenshot({ path: 'print.png' })
+  await page.goto('https://www.fctables.com/teams/palmeiras-191491/');
+  // await page.screenshot({ path: 'print.png' }) // Print this page
+
+  const teamInfo = await page.$$('#team-info');
+
+  // Collect all info of website
+  for(const infos of teamInfo) {
+    const teamName = await page.evaluate(el => el.innerText, infos);
+    console.table(teamName);
+  };
 
   await browser.close();
 
